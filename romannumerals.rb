f8a51f7503a7a3adfd01161885d5e1353c0d2a6a
Hannah <hannah_wight@hotmail.com>
2014-02-26 17:42:48 +0000

    Updated Roman Numeral files

--- romannumerals.rb
+++ romannumerals.rb
@@ -1,40 +1,71 @@
 
-
-
-def numeral_roman_index(number)
-	numerals = {:1 => "I", :5 => "V", :10 => "X", :50 => "L", :100 => "C", :500 => "D", :1000 => "M"}
-	numerals[number.to_s.to_sym]
+def numeral_roman_index(number) 
+  numerals = {1000 => "M", 500 => "D", 100 => "C", 50 => "L", 10 => "X", 5 => "V", 1 => "I"}
+  numerals[number]
 end
 
 
-def number_divided_by_thousand(number)
-	number / 1000
+def remainder(divisor, number)
+ 	r = number % divisor
+	number_divided_by(divisor, r)
+	r
 end
 
-def remainder_thousand(number)
-	number % 1000
+
+def number_divided_by(divisor, r)
+	r / divisor
 end
 
-# def number_divided_by_five_hundred?(number)
-# 	number / 500
-# end
 
-# def number_divided_by_hundred?(number)
-# 	number / 100
-# end
+def remainder_array(number)
+	r_array = []
+	i = 0
+	while r_array[6] == nil
+		divisors = [1000, 500, 100, 50, 10, 5, 1]
+		r_array[i] = remainder(divisors[i], number)
+		i += 1
+	end
+	r_array
+end
 
-# def number_divided_by_fifty?(number)
-# 	number / 50
-# end
 
-# def number_divided_by_ten?(number)
-# 	number / 10
-# end
+def division_array(number)
+	d_array = []
+	d_array[0] = number / 1000
+	i = 1
+	while d_array[6] == nil
+		array = remainder_array(number)
+		divisors = [1000, 500, 100, 50, 10, 5, 1]
+		d_array[i] =  array[i-1] / divisors[i]
+		i += 1
+	end
+	d_array
+end
 
-# def number_divided_by_five?(number)
-# 	number / 5
-# end
+def array_conversion(number)
+	roman_result_array = []
+	d_array = division_array(number) # [1, 1, 4, 1, 4, 1, 2]
+	romans_index = ['M', 'D', 'C', 'L', 'X', 'V', 'I']
+	i = 0
+	while i <= 6
+		roman_result_array[i] = "#{romans_index[i]}" * d_array[i]
+		i += 1
+	end
+	roman_result_array
+	
+end
 
-# def number_divided_by_one?(number)
-# 	number / 1
-# end
+def fours_and_nines(number)
+	array = array_conversion(number) # returns: roman_result_array
+	if array[2] == "CCCC"
+		array[2] = "CD"
+	end
+	if array[4] == "XXXX"
+		array[4] = "XL"
+	end
+	if array[6] == "IIII"
+		array[6] = "IV"
+	end
+	array
+end
+	#['M', 'D', 'CD', "L", "XL", "V", "II"]
--- romannumerals_spec.rb
+++ romannumerals_spec.rb
@@ -6,76 +6,112 @@ require './romannumerals'
 
 
 describe "Roman Numerals" do
-	context 'knows that' do
-		it ' the numeral to roman index is' do
-			expect(numeral_roman_index(1)).to eq("I")
-		end
-	end
+  context 'knows that' do
+
+    it ' should return I for 1' do
+      expect(numeral_roman_index(1)).to eq("I")
+    end
+
+    it ' should return V for 5' do
+      expect(numeral_roman_index(5)).to eq("V")
+    end
+
+    it ' should return X for 10' do
+      expect(numeral_roman_index(10)).to eq("X")
+    end
+
+    it ' should return L for 50' do
+      expect(numeral_roman_index(50)).to eq("L")
+    end
+
+    it ' should return C for 100' do
+      expect(numeral_roman_index(100)).to eq("C")
+    end
+
+    it ' should return D for 500' do
+      expect(numeral_roman_index(500)).to eq("D")
+    end
+
+    it ' should return M for 1000' do
+      expect(numeral_roman_index(1000)).to eq("M")
+    end
+  end
 
 	context ' calculates that' do
-		it ' the number of Ms' do
-			expect(number_divided_by_thousand(2001)).to eq(2) 
+		it ' the number of Ms is' do
+			expect(number_divided_by(1000, 1997)).to eq(1) 
 		end
 
 		it ' the remainder from 1000 is' do
-			expect(remainder_thousand(2001)).to eq(1)
+			expect(remainder(1000, 1997)).to eq(997)
 		end
 
+		it ' the number of Ds is' do
+			expect(number_divided_by(500, 997)).to eq(1) 
+		end
 
-		# it ' ' do
-		# 	expect that the number / 500 will give the number of Ds
-		# end
+		it ' the remainder from 500 is' do
+			expect(remainder(500, 997)).to eq(497)
+		end
 
-		# it ' ' do
-		# 	expect that the number / 100 will give the number of Cs
-		# end
+		it ' the number of Cs is' do
+			expect(number_divided_by(100, 497)).to eq(4)
+		end
 
+		it ' the remainder from 100 is' do
+			expect(remainder(100, 497)).to eq(97)
+		end
+
+		it ' the number of Ls is' do
+			expect(number_divided_by(50, 97)).to eq(1)
+		end
+
+		it ' the remainder from 50 is' do
+			expect(remainder(50, 97)).to eq(47)
+		end
+
+		it ' the number of Xs is' do
+			expect(number_divided_by(10, 47)).to eq(4)
+		end
+
+		it ' the remainder from 10 is' do
+			expect(remainder(10, 47)).to eq(7)
+		end
+
+		it ' the number of Vs is' do
+			expect(number_divided_by(5, 7)).to eq(1)
+		end
+
+		it ' the remainder from 5' do
+			expect(remainder(5, 7)).to eq(2)
+		end
+
+		it ' the number of Is is' do
+			expect(number_divided_by(1, 2)).to eq(2)
+		end
+
+		it ' the remainder from 1' do
+			expect(remainder(1, 2)).to eq(0)
+		end
+		
+		it ' the remainder array is' do	
+			expect(remainder_array(1997)).to eq([997, 497, 97, 47, 7, 2, 0])
+		end
+
+		it ' the division result array is' do
+			expect(division_array(1997)).to eq([1, 1, 4, 1, 4, 1, 2])
+		end
+	end
+
+	context ' converts' do
+		it ' the numeral array to roman numerals array' do
+			expect(array_conversion(1997)).to eq(['M', 'D', 'CCCC', 'L', 'XXXX', 'V', 'II'])
+		end
+
+		it ' 4s and 9s in the roman numeral array' do
+			expect(fours_and_nines(1997)).to eq(['M', 'D', 'CD', "L", "XL", "V", "II"])
+		end
 
 	end
+
 end
-
-
-
-
-# 		it something
-# 			#expect something
-# 		end
-
-# 		it something
-# 			#expect something
-# 		end
-# 	end
-
-# 	context ''
-# 		it something
-# 			#expect something
-# 		end
-# 	end
-# end
-
-
-#if number>1000 
-#if 1000>number>500
-#if 500>number>100
-#if 100>number>50
-#if 50>number>10
-#if 10>number>5 
-#if 5>number>1
-
-
-# 27 /10 = (number of 10s)
-# 27%10 = remainder
-# remainder/5 = (number of 5s)
-# remainder%5 = remainder
-# remainder/1 = number of 1s 
-# remainder%1 = remainder
-
-# 893/500 = number of 500s
-# 893 % 500
-# remainder / 100
-# remainder % 100
-# remainder / 50
-
-
-
-# unless 
